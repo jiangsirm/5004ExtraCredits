e8b011f068cd231ab1466cb47e9b1a5bb3feb64e
yuchen jiang <konsteintinj@yuchendeMBP.hsd1.wa.comcast.net>
2023-08-06 15:36:01 -0700

    displayPart

--- ExtraCredits/src/main/java/com/example/extracredits/HelloController.java
+++ ExtraCredits/src/main/java/com/example/extracredits/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.extracredits;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label addText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onAddButtonClick() {
+        addText.setText("Task Added!");
+        Duration textShow = Duration.seconds(1.5);
+        Timeline timeline = new Timeline(new KeyFrame(textShow, event -> addText.setText("")));
+        timeline.play();
     }
 }
