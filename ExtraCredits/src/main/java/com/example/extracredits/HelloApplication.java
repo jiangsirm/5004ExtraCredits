3fd848a7f66805c3d21be22c60a80df057b70c6b
yuchen jiang <konsteintinj@yuchendeMBP.hsd1.wa.comcast.net>
2023-08-06 12:34:26 -0700

    first try

--- ExtraCredits/src/main/java/com/example/extracredits/HelloApplication.java
+++ ExtraCredits/src/main/java/com/example/extracredits/HelloApplication.java
@@ -2,7 +2,12 @@ package com.example.extracredits;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +16,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
